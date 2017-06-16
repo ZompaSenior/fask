4ac353cffe88d7c2a8f57aeaf726cc24de827dbc
Luca Zomparelli <l.zomparelli@spal.it>
2017-06-16 12:42:24 +0200

    Added filter for progect supervisor, and changed parameter way, to GET form.

--- fask/src/fask_dj/dashboard/views.py
+++ fask/src/fask_dj/dashboard/views.py
@@ -3,22 +3,40 @@ from django.template import loader
 from django.http import HttpResponse
 from project.models import Project, ProjectState
 
-def dashboard(request, show_completed = 'false'):
+def dashboard(request):
     template = loader.get_template('dashboard/dashboard.html')
+
+    values = ('false', 'true')
     
+    tmp_show_completed = request.GET.get('show_completed')
     try:
-        values = ('false', 'true')
-        index = values.index(show_completed.lower())
+        index = values.index(tmp_show_completed.lower())
         tmp_show_completed = bool(index)
     except ValueError:
         tmp_show_completed = False
     except AttributeError:
         tmp_show_completed = False
     
-    if(tmp_show_completed):
+    tmp_filter_supervisor = request.GET.get('filter_supervisor')
+    try:
+        index = values.index(tmp_filter_supervisor.lower())
+        tmp_filter_supervisor = bool(index)
+    except ValueError:
+        tmp_filter_supervisor = False
+    except AttributeError:
+        tmp_filter_supervisor = False
+    
+    if(tmp_show_completed and not tmp_filter_supervisor):
         projects = Project.objects.all()
     else:
-        projects = Project.objects.exclude(state__description='99 - Completato')
+        print(request.user.username)
+        if(tmp_show_completed):
+            projects = Project.objects.filter(supervisor__username=request.user.username)
+        else:
+            if(tmp_filter_supervisor):
+                projects = Project.objects.filter(supervisor__username=request.user.username).exclude(state__description='99 - Completato')
+            else:
+                projects = Project.objects.exclude(state__description='99 - Completato')
         
-    context = {'projects': projects, 'show_completed': tmp_show_completed}
+    context = {'projects': projects, }
     return HttpResponse(template.render(context, request))
